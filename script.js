41b19b680d551c1f86d6b9f1556dafa697c24153
TheArk-KarimMenem <karim.menem@thearknetworks.com>
2025-11-14 20:13:35 +0200

    minor fixes

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const startBtn = document.getElementById('startBtn');
     const apologyContainer = document.getElementById('apologyContainer');
+    const backgroundOverlay = document.getElementById('backgroundOverlay');
     
     startBtn.addEventListener('click', () => {
-       
+        backgroundOverlay.classList.remove('hidden');
+        setTimeout(() => {
+            backgroundOverlay.classList.add('show');
+        }, 10);
+        
         startBtn.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
         startBtn.style.opacity = '0';
         startBtn.style.transform = 'scale(0.5)';
